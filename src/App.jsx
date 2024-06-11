bd29446583c543da05f9c99ff5517ee474eec40c
minhajul <dev.minhajul@gmail.com>
2024-06-11 16:35:18 +0600

    Updated modal implementation for better functionality. Added custom colors to Tailwind configuration

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <main>
       <section className="text-center">
-        <h1 className="text-3xl font-semibold">Vite + React</h1>
+        <h1 className="text-3xl  font-semibold">Vite + React</h1>
         <div className="card">
           <button
             className="px-5 py-1 text-lg rounded-sm bg-green-300 text-black"
@@ -30,6 +30,15 @@ function App() {
         <BaseModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
           <h1>Modal Content</h1>
           <p>This is the content inside the modal</p>
+          <p>
+            You can pass any <i>JSX</i>
+          </p>
+          <h1>Modal Content</h1>
+          <p>
+            This is the content insidis is the content insidis is the content
+            insidis is the content insidis is the content insidis is the content
+            inside the modal
+          </p>
           <p>
             You can pass any <i>JSX</i>
           </p>
